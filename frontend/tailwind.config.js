822712f90edb144b07f23875c19315a414d23ca7
bada308 <203190@jnu.ac.kr>
2024-02-13 18:19:33 +0900

    feat: primary color 정의
    - color code : #2160FF

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2160FF",
+      },
+    },
   },
   plugins: [],
 };
