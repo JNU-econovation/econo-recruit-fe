616978f15aa06d4e1465fd799f1d9a7e0263f4c5
박건규 <geongyu09@gmail.com>
2024-02-21 21:12:26 +0900

    feat: className 수정

--- frontend/components/common/navbar/LogoutBtn.tsx
+++ frontend/components/common/navbar/LogoutBtn.tsx
@@ -10,7 +10,7 @@ export default function LogoutBtn() {
   };
   return (
     <button
-      className="ml-4 text-sm border-b-2 border-white hover:border-secondary-200 hover:text-secondary-200 transition ease-in-out duration-150"
+      className="ml-4 text-sm border-b-2 border-white text-secondary-100 hover:border-dark hover:text-dark transition ease-in-out duration-150"
       onClick={logout}
     >
       로그아웃 &rarr;
