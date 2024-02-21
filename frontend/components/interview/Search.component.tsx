11eb03cd4b2f86a919cb6e8222b0d83eaf6e78a3
baegyeong <beth0929@naver.com>
2024-02-21 14:42:53 +0900

    feat: input의 onChange 함수 정의 (handleSearch)
    - useDebounceCallback 사용

--- frontend/components/interview/Search.component.tsx
+++ frontend/components/interview/Search.component.tsx
@@ -1,6 +1,25 @@
+"use client";
+
 import Icon from "../common/Icon";
+import { useDebouncedCallback } from "use-debounce";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 const InterviewSearch = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(Object.fromEntries(searchParams));
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <form className="flex items-center py-2 px-4 bg-primary-100 rounded-2xl gap-2">
       <button>
@@ -10,6 +29,9 @@ const InterviewSearch = () => {
         className="p-2 color-secondary-100 bg-transparent outline-none"
         type="text"
         placeholder="search"
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
       ></input>
     </form>
   );
