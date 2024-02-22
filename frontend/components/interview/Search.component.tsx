bb12e6d238e4e67862162e73b1a6f0f3c2c18785
baegyeong <beth0929@naver.com>
2024-02-22 11:47:26 +0900

    fix: button 태그 제거

--- frontend/components/interview/Search.component.tsx
+++ frontend/components/interview/Search.component.tsx
@@ -22,9 +22,7 @@ const InterviewSearch = () => {
 
   return (
     <form className="flex items-center py-2 px-4 bg-primary-100 rounded-2xl gap-2">
-      <button>
-        <Icon icon="search" />
-      </button>
+      <Icon icon="search" />
       <input
         className="p-2 color-secondary-100 bg-transparent outline-none"
         type="search"
