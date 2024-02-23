e17f58825232d4661aadd4eec4b02d855e80d973
bada308 <203190@jnu.ac.kr>
2024-02-23 16:32:42 +0900

    feat: interviewer.ts에 record API 요청 함수 삭제 - interviewer.ts에 "면접 영상" 관련 API 제거

--- frontend/src/apis/interview/interviewer.ts
+++ frontend/src/apis/interview/interviewer.ts
@@ -1,30 +1,5 @@
 import { https } from "@/src/functions/axios";
 
-// FIXME: record.ts와 겹치는 API 삭제
-export interface InterviewRes {
-  url: string;
-  record: string;
-}
-
-export const getInterviewRecord = async (id: string) => {
-  const { data } = await https.get<InterviewRes>(`/records`, {
-    params: { applicantId: id },
-  });
-  return data;
-};
-
-export interface interviewReqBody {
-  applicantId: string;
-  url: string;
-  record: string;
-}
-
-export const postInterviewRecord = async (recode: interviewReqBody) => {
-  const { data } = await https.post<interviewReqBody>(`/records`, recode);
-
-  return data;
-};
-
 export interface InterviewerReq {
   id: number;
   name: string;
