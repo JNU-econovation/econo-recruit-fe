12b804fc4549c002f8337ce3e5e12faae45569e9
bada308 <203190@jnu.ac.kr>
2024-02-24 15:23:08 +0900

    feat: interview finder 함수 제거

--- frontend/src/functions/finder.ts
+++ frontend/src/functions/finder.ts
@@ -11,10 +11,11 @@ export const applicantDataFinder = (
   return data === "" ? "" : JSON.parse(data);
 };
 
-export const interviewDataFinder = (
-  interviewData: InterviewRes[],
-  name: string
-) => {
-  const data = interviewData.find((req) => req.url === name)?.record ?? "";
-  return data === "" ? "" : JSON.parse(data);
-};
+// interview 응답 형식은 변경이 잦지 않은데 finder 함수가 필요할까??
+// export const interviewDataFinder = (
+//   interviewData: InterviewRes[],
+//   name: string
+// ) => {
+//   const data = interviewData.find((req) => req.url === name)?.record ?? "";
+//   return data === "" ? "" : JSON.parse(data);
+// };
