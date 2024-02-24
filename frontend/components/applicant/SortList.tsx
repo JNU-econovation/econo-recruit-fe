f0adfdc07f7f305c80f6289177edfcb131098c8e
bada308 <203190@jnu.ac.kr>
2024-02-24 23:12:23 +0900

    fix: 오타를 수정합니다.

--- frontend/components/applicant/SortList.tsx
+++ frontend/components/applicant/SortList.tsx
@@ -5,13 +5,13 @@ import { useSearchParams } from "next/navigation";
 
 const ApplicantSortList = () => {
   const queryClient = useQueryClient();
-  const searchParmas = useSearchParams();
+  const searchParams = useSearchParams();
   const orderMenu = [
     { type: "newest", string: "최신순" },
     { type: "name", string: "이름순" },
   ];
-  const order = searchParmas.get("order") ?? "newest";
-  const pageIndex = searchParmas.get("page") || "1";
+  const order = searchParams.get("order") ?? "newest";
+  const pageIndex = searchParams.get("page") || "1";
 
   const onOrderChange = () => {
     queryClient.invalidateQueries({
