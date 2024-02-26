d339ee86b29ae433d329ce867655a51a92c619dd
박건규 <geongyu09@gmail.com>
2024-02-26 17:19:34 +0900

    feat: 6번째 에러 페이지 추가

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,9 +4,10 @@ import ErrorPage1 from "@/components/common/error/ErrorPage1";
 import ErrorPage3 from "@/components/common/error/ErrorPage3";
 import ErrorPage4 from "@/components/common/error/ErrorPage4";
 import ErrorPage5 from "@/components/common/error/ErrorPage5";
+import ErrorPage6 from "@/components/common/error/ErrorPage6";
 
 const NotFound = () => {
-  return <ErrorPage5 />;
+  return <ErrorPage6 />;
 };
 
 export default NotFound;
