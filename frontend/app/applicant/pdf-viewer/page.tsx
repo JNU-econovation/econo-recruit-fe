aa7047ca3d71a45c7896254715d0833a40bdb181
baegyeong <beth0929@naver.com>
2024-02-26 21:32:00 +0900

    refactor: pdf viewer page에 prefetchQuey를 적용한다.

--- frontend/app/applicant/pdf-viewer/page.tsx
+++ frontend/app/applicant/pdf-viewer/page.tsx
@@ -1,12 +1,30 @@
-import dynamic from "next/dynamic";
+import { dehydrate, Hydrate } from "@tanstack/react-query";
+import getQueryClient from "@/src/functions/getQueryClient";
+import { getApplicationById } from "@/src/apis/applicant";
+import ApplicationPdfViewer from "@/components/applicant/PdfViewr.component";
 
-const ApplicationPdfViewerPage = () => {
-  const ApplicantPdfViewr = dynamic(
-    () => import("@/components/applicant/PdfViewr.component"),
-    { ssr: false }
+interface ApplicationPdfViewerPageProps {
+  searchParams: {
+    applicantId: string;
+  };
+}
+const ApplicationPdfViewerPage = async ({
+  searchParams,
+}: ApplicationPdfViewerPageProps) => {
+  const { applicantId } = searchParams;
+
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["applicant", applicantId],
+    queryFn: () => getApplicationById(applicantId),
+  });
+  const dehydrateState = dehydrate(queryClient);
+
+  return (
+    <Hydrate state={dehydrateState}>
+      <ApplicationPdfViewer />
+    </Hydrate>
   );
-
-  return <ApplicantPdfViewr />;
 };
 
 export default ApplicationPdfViewerPage;
