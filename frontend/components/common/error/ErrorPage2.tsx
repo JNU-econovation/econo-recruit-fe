9fab44805db71042e08a5ab76ac5e761115b48e1
박건규 <geongyu09@gmail.com>
2024-02-26 17:44:24 +0900

    feat: 불필요한 컴포넌트 제거

--- frontend/components/common/error/ErrorPage2.tsx
+++ frontend/components/common/error/ErrorPage2.tsx
@@ -11,8 +11,8 @@ const ErrorPage1 = () => {
             <h2 className="text-black text-5xl font-medium mt-4">
               오류가 발생했군요.
             </h2>
-            <p className="text-dark mt-7 text-xl font-semibold">
-              <h3 className="text-3xl font-semibold my-2">Please Call Us.</h3>
+            <h3 className="text-3xl font-semibold mt-8">Please Call Us.</h3>
+            <p className="text-dark text-xl font-semibold my-2">
               우리에게 연락줘요. 빠르게 처리해드립니다.
             </p>
           </div>
