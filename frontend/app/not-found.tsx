aa17f2783af06536d74bfc674f5fc03111b1e8bd
박건규 <geongyu09@gmail.com>
2024-02-26 17:09:54 +0900

    feat: 5번째 에러 페이지 추가

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -3,9 +3,10 @@ import ErrorPage2 from "@/components/common/error/ErrorPage2";
 import ErrorPage1 from "@/components/common/error/ErrorPage1";
 import ErrorPage3 from "@/components/common/error/ErrorPage3";
 import ErrorPage4 from "@/components/common/error/ErrorPage4";
+import ErrorPage5 from "@/components/common/error/ErrorPage5";
 
 const NotFound = () => {
-  return <ErrorPage4 />;
+  return <ErrorPage5 />;
 };
 
 export default NotFound;
---
+++ frontend/components/common/error/ErrorPage5.tsx
@@ -0,0 +1,40 @@
+import { managementTeam } from "@/src/constants";
+
+const ErrorPage5 = () => {
+  return (
+    <div className="absolute h-full left-0 top-0 w-full flex justify-center items-center">
+      <div className="flex items-end gap-20">
+        <img src="/images/error5.png" alt="error page" />
+        <div>
+          <div className="flex flex-col">
+            <h1 className="text-black text-7xl font-bold">이런!</h1>
+            <h2 className="text-black text-5xl font-medium mt-4">
+              오류가 발생했군요.
+            </h2>
+            <h3 className="text-2xl font-semibold mt-12">
+              그렇다면, 우리를 찾아줘요.
+            </h3>
+          </div>
+          <div className="flex items-start flex-col mt-3">
+            {managementTeam.position.map((occupation) => (
+              <div className="text-secondary-100 text-center" key={occupation}>
+                <span>{occupation}: </span>
+                {managementTeam.member.map(
+                  ({ generation, name, position }, index) =>
+                    position === occupation && (
+                      <span
+                        className=""
+                        key={index}
+                      >{`${generation}기 ${name}`}</span>
+                    )
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage5;
