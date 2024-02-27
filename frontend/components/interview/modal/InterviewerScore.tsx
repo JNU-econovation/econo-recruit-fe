184a1a3455e5a8090ff28eb9d84c05e1407c8175
bada308 <203190@jnu.ac.kr>
2024-02-27 15:41:13 +0900

    feat: 다른 TF의 점수를 확인할 수 있는 컴포넌트를 구현한다.

---
+++ frontend/components/interview/modal/InterviewerScore.tsx
@@ -0,0 +1,66 @@
+import { Score, ScoreKeyword } from "@/src/constants/applicant/27";
+
+interface InterviewerScoreProps {
+  interviewers: {
+    [key: string]: Score[];
+  };
+}
+
+interface InterviewerScoresItemProps {
+  name: string;
+  scores: Score[];
+}
+
+const MOCK_FIELD_NAME: ScoreKeyword[] = [
+  "실천력",
+  "동아리 활동의지",
+  "협업",
+  "베풀려는 마음",
+];
+
+const InterviewerScores = ({ interviewers }: InterviewerScoreProps) => {
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      <InterviewerScoresHeader />
+      {Object.entries(interviewers).map(([name, scores]) => (
+        <InterviewerScoresItem name={name} scores={scores} />
+      ))}
+    </div>
+  );
+};
+
+const InterviewerScoresHeader = () => {
+  return (
+    <div className="grid grid-cols-[5rem_1fr] gap-12 w-full">
+      <div></div>
+      <div className="flex w-full justify-between">
+        {MOCK_FIELD_NAME.map((fieldName) => (
+          <span className="text-dark w-[4.5rem] text-sm text-center break-keep">
+            {fieldName}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const InterviewerScoresItem = ({
+  name,
+  scores,
+}: InterviewerScoresItemProps) => {
+  return (
+    <div className="grid grid-cols-[5rem_1fr] gap-12 w-full">
+      <div className="w-fit px-4 py-2 bg-primary-200 rounded-3xl">
+        <span className="text-primary-500 text-sm font-semibold">{name}</span>
+      </div>
+      <div className="flex w-full justify-between">
+        {scores.map((score) => (
+          <span className="text-dark text-lg w-[4.5rem] text-center">
+            {score.score}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default InterviewerScores;
