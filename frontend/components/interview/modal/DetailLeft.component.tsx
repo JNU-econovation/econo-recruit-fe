95053e01c253d796b858d519832292ddc109f6d4
bada308 <203190@jnu.ac.kr>
2024-02-27 17:35:16 +0900

    fix: InterviewDetailLeftComponent에 InterViewScore 컴포넌트 대신 InterviewScore 컴포넌트를 추가한다.

--- frontend/components/interview/modal/DetailLeft.component.tsx
+++ frontend/components/interview/modal/DetailLeft.component.tsx
@@ -1,43 +1,11 @@
 import InterviewUserComponent from "./User.component";
 import { getInterviewRecord } from "@/src/apis/interview";
-import { getScore } from "@/src/apis/score";
-import InterviewAvgComponent from "@/components/interview/modal/AvgScore.component";
-import InterviewScoreComponent from "./Score.component";
 import InterviewEditComponent from "./Edit.component";
 import InterviewUploadComponent from "./Upload.component";
 import { useQuery } from "@tanstack/react-query";
 import { useAtomValue } from "jotai";
 import { interViewApplicantIdState } from "@/src/stores/interview/Interview.atom";
-
-const InterViewScore = () => {
-  const applicantId = useAtomValue(interViewApplicantIdState);
-  const {
-    data: scoreData,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["score", applicantId],
-    queryFn: () => getScore(applicantId),
-  });
-
-  if (!scoreData || isLoading) {
-    return <div>로딩중...</div>;
-  }
-
-  if (isError) {
-    return <div>에러 발생</div>;
-  }
-
-  return (
-    <>
-      <InterviewAvgComponent
-        totalAverage={scoreData.totalAverage}
-        average={scoreData.scoreVo.average}
-      />
-      <InterviewScoreComponent score={scoreData} />
-    </>
-  );
-};
+import InterviewScore from "./score/InterviewScore";
 
 const InterViewEditorOrUploader = () => {
   const applicantId = useAtomValue(interViewApplicantIdState);
@@ -58,7 +26,7 @@ const InterviewDetailLeftComponent = () => {
   return (
     <>
       <InterviewUserComponent />
-      <InterViewScore />
+      <InterviewScore />
       <InterViewEditorOrUploader />
     </>
   );
