7ba7cd58d52edf77a5824217c921a0a2a73bfe32
bada308 <203190@jnu.ac.kr>
2024-02-27 16:00:15 +0900

    feat: score 관련 Formmater을 구현한다.

---
+++ frontend/src/functions/formatter.ts
@@ -0,0 +1,23 @@
+import { Score, ScoreKeyword } from "../constants/applicant/27";
+
+export const scoreListToObject = (
+  scores: number[],
+  ScoreSequece: {
+    [key: number]: ScoreKeyword;
+  }
+): Score[] => {
+  if (scores.length === 0) {
+    return Object.values(ScoreSequece).map((fieldName) => ({
+      fieldName,
+      score: 0,
+    }));
+  }
+  return scores.map((score, index) => ({
+    fieldName: ScoreSequece[index],
+    score,
+  }));
+};
+
+export const scoreObjectToList = (scores: Score[]) => {
+  return scores.reduce((acc: number[], cur: Score) => [...acc, cur.score], []);
+};
