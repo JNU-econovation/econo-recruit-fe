e86cfed4eff18161b26be252efdd020603f489e9
bada308 <203190@jnu.ac.kr>
2024-02-27 15:38:48 +0900

    fix: 평균 점수를 표기하는 AvgScore 컴포넌트를 수정한다.

---
+++ frontend/components/interview/modal/AvgScore.tsx
@@ -0,0 +1,32 @@
+import ScoreCell from "./ScoreCell";
+import { Score } from "@/src/constants/applicant/27";
+
+interface AvgScoreProps {
+  totalAverage: number;
+  fieldAverages: Score[];
+}
+interface TotalAverageProps extends Omit<AvgScoreProps, "fieldAverages"> {}
+
+const TotalAverage = ({ totalAverage }: TotalAverageProps) => {
+  return (
+    <div className="w-full relative">
+      <p className="w-full text-5xl text-dark font-extrabold">{totalAverage}</p>
+      <div className="bg-primary-300 absolute w-full h-5 top-8 -z-10"></div>
+    </div>
+  );
+};
+
+const AvgScore = ({ totalAverage, fieldAverages }: AvgScoreProps) => {
+  return (
+    <div className="w-full grid grid-cols-[5rem_1fr] gap-12">
+      <TotalAverage totalAverage={totalAverage} />
+      <div className="flex w-full justify-between">
+        {fieldAverages.map(({ fieldName, score }) => (
+          <ScoreCell fieldName={fieldName} score={score} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AvgScore;
