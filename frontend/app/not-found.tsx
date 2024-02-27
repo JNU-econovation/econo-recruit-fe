fa0e64ee5451c9bc7bcd8d9c04d1d4cd085230b0
박건규 <geongyu09@gmail.com>
2024-02-27 18:18:58 +0900

    refactor: not-found 페이지 리팩토링

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,24 +1,25 @@
 "use client";
-import ErrorPage2 from "@/components/common/error/ErrorPage2";
 import ErrorPage1 from "@/components/common/error/ErrorPage1";
+import ErrorPage2 from "@/components/common/error/ErrorPage2";
 import ErrorPage3 from "@/components/common/error/ErrorPage3";
 import ErrorPage4 from "@/components/common/error/ErrorPage4";
 import ErrorPage5 from "@/components/common/error/ErrorPage5";
 import ErrorPage6 from "@/components/common/error/ErrorPage6";
 
-const NotFound = () => {
-  const errorPage = Math.floor(Math.random() * 6) + 1;
+const errorPages = [
+  ErrorPage1,
+  ErrorPage2,
+  ErrorPage3,
+  ErrorPage4,
+  ErrorPage5,
+  ErrorPage6,
+];
 
-  return (
-    <>
-      {errorPage === 1 && <ErrorPage1 />}
-      {errorPage === 2 && <ErrorPage2 />}
-      {errorPage === 3 && <ErrorPage3 />}
-      {errorPage === 4 && <ErrorPage4 />}
-      {errorPage === 5 && <ErrorPage5 />}
-      {errorPage === 6 && <ErrorPage6 />}
-    </>
-  );
+const NotFound = () => {
+  const errorPage = Math.floor(Math.random() * 6);
+  const PageComponent = errorPages[errorPage] || ErrorPage1;
+
+  return <PageComponent />;
 };
 
 export default NotFound;
