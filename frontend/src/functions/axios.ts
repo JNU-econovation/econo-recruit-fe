97f4aedb6891dc904a7b51f04feeba40bb491974
bada308 <203190@jnu.ac.kr>
2024-03-03 01:31:12 +0900

    feat: response 인터셉터에 에러 코드 처리 로직을 추가한다

--- frontend/src/functions/axios.ts
+++ frontend/src/functions/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import errorConfig from "./errorConfig";
 
 const https = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -19,12 +20,21 @@ https.interceptors.response.use(
     return response;
   },
   (error) => {
-    if (error.response.status === 401 || error.response.status === 403) {
+    const { data, status: errorStatus } = error.response;
+    const { code: errorCode, reason: errorReason } = data;
+    const errorMessage =
+      errorConfig[errorCode].message || errorReason || error.message;
+
+    error.message = errorMessage;
+
+    if (errorStatus === 401 || errorStatus === 403) {
       localStorage.removeItem("accessToken");
       localStorage.removeItem("refreshToken");
       alert("로그인이 필요합니다.");
       window.location.href = "/signin";
     }
+
+    return Promise.reject(error);
   }
 );
 
