744f43522e70c58fbde02a948224d836f3b0bfd1
baegyeong <beth0929@naver.com>
2024-03-03 15:59:37 +0900

    fix: 27기 파일들로 import문 수정

--- frontend/src/stores/applicant/index.ts
+++ frontend/src/stores/applicant/index.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 
-const applicantQuestions = require(`@/src/constants/applicant/26`)
+const applicantQuestions = require(`@/src/constants/applicant/27`)
   .APPLICANT as ApplicantNode[];
 
 export const applicantQuestionsAtom = atom(applicantQuestions);
