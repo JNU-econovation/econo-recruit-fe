a90b58f92ce183f792f0c2e8ff93027c5e70d3d0
bada308 <203190@jnu.ac.kr>
2024-03-03 23:06:41 +0900

    feat: 입력 길이 제한 핸들러 추가 및 글자수 표기

--- frontend/components/application/applicationNode/Textarea.component.tsx
+++ frontend/components/application/applicationNode/Textarea.component.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Txt from "@/components/common/Txt.component";
+import { MAX_TEXT_LENGTH } from "@/src/constants";
 import {
   ApplicationNode,
   ApplicationTextarea,
@@ -15,6 +16,10 @@ const ApplicationTexarea = ({ data }: ApplicationTextareaProps) => {
   const textData = data as ApplicationTextarea;
   const [value, setValue] = useLocalStorage(textData.name, "");
 
+  const onInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    setValue(e.currentTarget.value.slice(0, MAX_TEXT_LENGTH));
+  };
+
   return (
     <>
       {textData.title && (
@@ -25,16 +30,17 @@ const ApplicationTexarea = ({ data }: ApplicationTextareaProps) => {
           {textData.subtitle && <Txt>{` ${textData.subtitle}`}</Txt>}
         </label>
       )}
-      <textarea
-        className="my-2 border rounded-lg p-4 w-full resize-none"
-        rows={20}
-        name={textData.name}
-        value={value}
-        maxLength={1000}
-        onInput={(e) => {
-          setValue(e.currentTarget.value);
-        }}
-      />
+      <div className="relative">
+        <textarea
+          className="border rounded-lg px-4 py-6 w-full resize-none"
+          rows={20}
+          name={textData.name}
+          value={value}
+          maxLength={1000}
+          onInput={onInput}
+        />
+        <div className="absolute bottom-3 right-4 bg-white text-sm">{`(${value.length}/${MAX_TEXT_LENGTH})`}</div>
+      </div>
     </>
   );
 };
