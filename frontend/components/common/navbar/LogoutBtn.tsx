af6905c52043cc5e694de4ed9de44dcd65c6558e
박건규 <geongyu09@gmail.com>
2024-03-06 11:59:44 +0900

    feat: 로그아웃 핸들러 수정

--- frontend/components/common/navbar/LogoutBtn.tsx
+++ frontend/components/common/navbar/LogoutBtn.tsx
@@ -1,17 +1,17 @@
 "use client";
+import { signOut } from "@/src/apis/user";
 import { useRouter } from "next/navigation";
 
 export default function LogoutBtn() {
   const router = useRouter();
-  const logout = () => {
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
-    router.replace("/signin");
+  const onLogout = async () => {
+    await signOut();
+    router.push("/signin");
   };
   return (
     <button
       className="ml-4 text-sm border-b-2 border-white text-secondary-100 hover:border-dark hover:text-dark transition ease-in-out duration-150"
-      onClick={logout}
+      onClick={onLogout}
     >
       로그아웃 &rarr;
     </button>
