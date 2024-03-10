552a14653c22f8eca56c6e8d9c3b1b65a57a70db
baegyeong <beth0929@naver.com>
2024-03-10 21:51:42 +0900

    feat: useSearchQuery 훅 구현
    - react query를 사용하여 구현
    - 면접기록에 사용하는 데이터 형식 정의 (searchInterviewData)
    - 지원현황에 사용하는 데이터 형식 정의 (searchApplicantData)

---
+++ frontend/src/hooks/useSearchQuery.tsx
@@ -0,0 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { getSearchKeyword } from "@/src/apis/interview";
+
+export const useSearchQuery = (pageIndex: string) => {
+  const searchParams = useSearchParams();
+  const searchKeyword = searchParams.get("search") || "";
+
+  const { data: search } = useQuery({
+    queryKey: ["searchKeyWord", pageIndex, searchKeyword],
+    queryFn: () => getSearchKeyword(+pageIndex, searchKeyword),
+    enabled: !!searchKeyword,
+    select: (data) => data.answers,
+  });
+
+  const searchInterviewData = search?.map((value) => ({
+    id: value.id,
+    title: `[${value.field}] ${value.name}`,
+    subElements: [
+      value.field1.split('"').join(""),
+      value.field2.split('"').join(""),
+      `${value.grade.split('"').join("")} ${value.semester
+        .split('"')
+        .join("")}`,
+    ],
+  }));
+
+  const searchApplicantData = search?.map((value) => ({
+    id: value.id,
+    title: `[${value.field}] ${value.name}`,
+    subElements: [
+      value.field1.split('"').join(""),
+      value.field2.split('"').join(""),
+      `${value.grade.split('"').join("")} ${value.semester
+        .split('"')
+        .join("")}`,
+      new Date(Number(value.uploadData)).toLocaleString("ko-KR", {
+        dateStyle: "short",
+      }),
+    ],
+  }));
+
+  return { searchInterviewData, searchApplicantData };
+};
