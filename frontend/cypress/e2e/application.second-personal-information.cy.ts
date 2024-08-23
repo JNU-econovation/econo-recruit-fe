49cc01eb6ecc58b13333161d67af27dc71c4cd9c
minbo <smb5174@naver.com>
2024-08-23 22:54:04 +0900

    Test: 전공 입력 후 다음 버튼 클릭 ->  테스트 코드 작성

--- frontend/cypress/e2e/application.second-personal-information.cy.ts
+++ frontend/cypress/e2e/application.second-personal-information.cy.ts
@@ -1,3 +1,13 @@
 describe("2번째 인적사항 e2e 테스트", () => {
   beforeEach(() => cy.viewport(1200, 900));
+
+  it("전공 입력 후 다음 버튼 클릭", () => {
+    cy.goSecondPersonalInformation();
+    cy.get("input.my-2.border.rounded-lg.p-4.w-full")
+      .eq(0)
+      .type("컴퓨터정보통신공학과");
+    cy.get(
+      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
+    ).click();
+  });
 });
