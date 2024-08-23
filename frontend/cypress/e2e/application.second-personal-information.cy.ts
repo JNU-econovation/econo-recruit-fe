0f4763b429d202289077535a36803df5a6116e35
minbo <smb5174@naver.com>
2024-08-23 22:58:57 +0900

    Test: 전공, 복수전공, 부전공 입력 후 다음 버튼 클릭 -> 테스트 코드 작성

--- frontend/cypress/e2e/application.second-personal-information.cy.ts
+++ frontend/cypress/e2e/application.second-personal-information.cy.ts
@@ -32,4 +32,16 @@ describe("2번째 인적사항 e2e 테스트", () => {
       ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
     ).click();
   });
+
+  it("전공, 복수전공, 부전공 입력 후 다음 버튼 클릭", () => {
+    cy.goSecondPersonalInformation();
+    cy.get("input.my-2.border.rounded-lg.p-4.w-full")
+      .eq(0)
+      .type("컴퓨터정보통신공학과");
+    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(1).type("건축학과");
+    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(2).type("물리학과");
+    cy.get(
+      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
+    ).click();
+  });
 });
