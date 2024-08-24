dfd018c51a9efeba0de13dbe4a69b6f760e22406
minbo <smb5174@naver.com>
2024-08-24 13:37:22 +0900

    test: 두번째 인적사항 테스트 코드 변경 개발자가 알아보기 편하게 태그 식별자 변경 assertion 로직 추가

--- frontend/cypress/e2e/application.second-personal-information.cy.ts
+++ frontend/cypress/e2e/application.second-personal-information.cy.ts
@@ -1,67 +1,103 @@
 describe("2번째 인적사항 e2e 테스트", () => {
   beforeEach(() => cy.viewport(1200, 900));
 
-  it("전공 입력 후 다음 버튼 클릭", () => {
+  it("전공 입력 후 다음 버튼 클릭하면 기타 질문 사항으로 이동", () => {
     cy.goSecondPersonalInformation();
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full")
-      .eq(0)
+    cy.get("span")
+      .filter((index, element) => Cypress.$(element).text().trim() === "전공*")
+      .parent()
+      .next("input")
+      .should("exist")
       .type("컴퓨터정보통신공학과");
-    cy.get(
-      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
-    ).click();
+    cy.get("button").contains("다음").should("exist").click();
   });
 
-  it("전공, 복수전공 입력 후 다음 버튼 클릭", () => {
+  it("전공, 복수전공 입력 후 다음 버튼 클릭하면 기타 질문 사항으로 이동", () => {
     cy.goSecondPersonalInformation();
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full")
-      .eq(0)
+    cy.get("span")
+      .filter((index, element) => Cypress.$(element).text().trim() === "전공*")
+      .parent()
+      .next("input")
+      .should("exist")
       .type("컴퓨터정보통신공학과");
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(1).type("물리학과");
-    cy.get(
-      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
-    ).click();
+    cy.get("span")
+      .filter(
+        (index, element) => Cypress.$(element).text().trim() === "복수전공"
+      )
+      .parent()
+      .next("input")
+      .should("exist")
+      .type("건축학과");
+    cy.get("button").contains("다음").should("exist").click();
   });
 
-  it("전공, 부전공 입력 후 다음 버튼 클릭", () => {
+  it("전공, 부전공 입력 후 다음 버튼 클릭하면 기타 질문 사항으로 이동", () => {
     cy.goSecondPersonalInformation();
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full")
-      .eq(0)
+    cy.get("span")
+      .filter((index, element) => Cypress.$(element).text().trim() === "전공*")
+      .parent()
+      .next("input")
+      .should("exist")
       .type("컴퓨터정보통신공학과");
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(2).type("물리학과");
-    cy.get(
-      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
-    ).click();
+    cy.get("span")
+      .filter((index, element) => Cypress.$(element).text().trim() === "부전공")
+      .parent()
+      .next("input")
+      .should("exist")
+      .type("물리학과");
+    cy.get("button").contains("다음").should("exist").click();
   });
 
-  it("전공, 복수전공, 부전공 입력 후 다음 버튼 클릭", () => {
+  it("전공, 복수전공, 부전공 입력 후 다음 버튼 클릭하면 기타 질문 사항으로 이동", () => {
     cy.goSecondPersonalInformation();
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full")
-      .eq(0)
+    cy.get("span")
+      .filter((index, element) => Cypress.$(element).text().trim() === "전공*")
+      .parent()
+      .next("input")
+      .should("exist")
       .type("컴퓨터정보통신공학과");
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(1).type("건축학과");
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(2).type("물리학과");
-    cy.get(
-      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
-    ).click();
+    cy.get("span")
+      .filter(
+        (index, element) => Cypress.$(element).text().trim() === "복수전공"
+      )
+      .parent()
+      .next("input")
+      .should("exist")
+      .type("건축학과");
+    cy.get("span")
+      .filter((index, element) => Cypress.$(element).text().trim() === "부전공")
+      .parent()
+      .next("input")
+      .should("exist")
+      .type("물리학과");
+    cy.get("button").contains("다음").should("exist").click();
   });
 
-  it("아무것도 입력하지 않고 다음 버튼 클릭", () => {
+  it("아무것도 입력하지 않고 다음 버튼 클릭하면 '필수 질문을 작성해주세요.'라는 alert창이 보인다.", () => {
     cy.goSecondPersonalInformation();
-    cy.get(
-      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
-    ).click();
+    cy.get("button").contains("다음").should("exist").click();
     cy.on("window:alert", (text) => {
       console.log("Alert message:", text);
     });
   });
 
-  it("전공을 입력하지 않고 복수전공, 부전공을 입력 후 버튼 클릭", () => {
+  it("전공을 입력하지 않고 복수전공, 부전공을 입력 후 다음 버튼 클릭하면 '필수 질문을 작성해주세요.'라는 alert창이 보인다.", () => {
     cy.goSecondPersonalInformation();
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(1).type("건축학과");
-    cy.get("input.my-2.border.rounded-lg.p-4.w-full").eq(2).type("물리학과");
-    cy.get(
-      ".flex-1.rounded-md.flex.justify-center.items-center.p-4.bg-dark.text-white"
-    ).click();
+    cy.get("span")
+      .filter(
+        (index, element) => Cypress.$(element).text().trim() === "복수전공"
+      )
+      .parent()
+      .next("input")
+      .should("exist")
+      .type("건축학과");
+    cy.get("span")
+      .filter((index, element) => Cypress.$(element).text().trim() === "부전공")
+      .parent()
+      .next("input")
+      .should("exist")
+      .type("물리학과");
+    cy.get("button").contains("다음").should("exist").click();
     cy.on("window:alert", (text) => {
       console.log("Alert message:", text);
     });
