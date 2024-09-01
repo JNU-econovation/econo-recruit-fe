debae30ed1f0928993ce3bf0cf949e74a61af479
박건규 <geongyu09@gmail.com>
2024-09-01 17:28:28 +0900

    refactor: toLocaleDateString 적용
    #175"

--- frontend/components/application/applicationLayout/timeline/Timeline.component.tsx
+++ frontend/components/application/applicationLayout/timeline/Timeline.component.tsx
@@ -29,9 +29,14 @@ export const TimelineCell = ({
 
   return (
     <div className="w-full">
-      <Txt typography="h6" className="block pb-8">{`${
-        startTime.getMonth() + 1
-      }월 ${startTime.getDate()}일 (${convertDay(startTime.getDay())})`}</Txt>
+      <Txt
+        typography="h6"
+        className="block pb-8"
+      >{`${startTime.toLocaleDateString("ko-KR", {
+        month: "long",
+        day: "numeric",
+        weekday: "short",
+      })}`}</Txt>
       <div className="w-full flex">
         {dates.map((date, index) => (
           <TimelineRow
