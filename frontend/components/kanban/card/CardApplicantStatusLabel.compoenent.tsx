93fba7479ba2d0c828a1e27240e9295db7d156b8
minbo <smb5174@naver.com>
2024-09-13 15:58:53 +0900

    refactor: 반복되는 부분 상수화하는 방법으로 변경

--- frontend/components/kanban/card/CardApplicantStatusLabel.compoenent.tsx
+++ frontend/components/kanban/card/CardApplicantStatusLabel.compoenent.tsx
@@ -2,29 +2,32 @@ interface KanbanCardApplicantStatusLabelProps {
   passState: "non-passed" | "first-passed" | "final-passed";
 }
 
+const labelConfig = {
+  "non-passed": {
+    backgroundColor: "bg-gray-300",
+    label: "처리중",
+  },
+  "first-passed": {
+    backgroundColor: "bg-lime-300",
+    label: "1차 합격",
+  },
+  "final-passed": {
+    backgroundColor: "bg-blue-300",
+    label: "최종 합격",
+  },
+};
+
 const KanbanCardApplicantStatusLabel = ({
   passState,
 }: KanbanCardApplicantStatusLabelProps) => {
-  switch (passState) {
-    case "non-passed":
-      return (
-        <div className="bg-gray-300 text-xs px-[10px] py-[5px] rounded-[10px]">
-          처리중
-        </div>
-      );
-    case "first-passed":
-      return (
-        <div className="bg-lime-300 text-xs px-[10px] py-[5px] rounded-[10px]">
-          1차 합격
-        </div>
-      );
-    case "final-passed":
-      return (
-        <div className="bg-blue-300 text-xs px-[10px] py-[5px] rounded-[10px]">
-          최종 합격
-        </div>
-      );
-  }
+  const { backgroundColor, label } = labelConfig[passState];
+  return (
+    <div
+      className={`${backgroundColor} text-xs px-[10px] py-[5px] rounded-[10px]`}
+    >
+      {label}
+    </div>
+  );
 };
 
 export default KanbanCardApplicantStatusLabel;
