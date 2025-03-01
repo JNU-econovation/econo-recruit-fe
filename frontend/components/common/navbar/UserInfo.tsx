07873410b7208bfb94267143dac81337ac3087e7
조인식 <imsif42@naver.com>
2025-03-01 11:10:13 +0900

    fix: 아이콘만 hover했을 때 content 보여지게 수정

--- frontend/components/common/navbar/UserInfo.tsx
+++ frontend/components/common/navbar/UserInfo.tsx
@@ -25,17 +25,19 @@ const NavbarUserInfo = () => {
   } = useQuery(["user"], () => getMyInfo());
 
   const renderContent = (content: React.ReactNode) => (
-    <div className="group flex items-center">
-      <div className="w-12 h-12 bg-white rounded-full shadow-md flex items-center justify-center overflow-hidden">
-        <Image
-          src={econoLogo}
-          alt="econo-Logo"
-          className="w-full h-full object-contain"
-        />
-      </div>
+    <div className="flex items-center">
+      <div className="relative group">
+        <div className="w-12 h-12 bg-white rounded-full shadow-md flex items-center justify-center overflow-hidden">
+          <Image
+            src={econoLogo}
+            alt="Econo 3D Logo"
+            className="w-full h-full object-contain"
+          />
+        </div>
 
-      <div className="ml-2 transition-all duration-300 ease-in-out opacity-0 transform -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0">
-        {content}
+        <div className="absolute left-full top-0 ml-2 transition-all duration-300 ease-in-out opacity-0 transform -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 pointer-events-none whitespace-nowrap">
+          {content}
+        </div>
       </div>
     </div>
   );
