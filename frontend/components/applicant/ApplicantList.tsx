4908cccd683a35ff8d5c774468f8094ca8bb0d2a
2yunseong <dbsdltjd123@gmail.com>
2025-03-18 01:44:18 +0900

    feat: client prefetch pagination

--- frontend/components/applicant/ApplicantList.tsx
+++ frontend/components/applicant/ApplicantList.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import useApplicantPaginationParams from "../../src/hooks/applicant/useApplicantPaginationParams";
 import { getApplicantByPageWithGeneration } from "../../src/apis/applicant";
 import LoadingSpinner from "../common/LoadingSpinner";
 import ApplicantBoard from "./Board";
 import ApplicantPageNavbar from "./PageNavbar";
+import { useEffect } from "react";
 
 interface ApplicantListProps {
   generation: string;
@@ -13,7 +14,11 @@ interface ApplicantListProps {
 
 const ApplicantList = ({ generation }: ApplicantListProps) => {
   const { pageIndex, order, searchKeyword } = useApplicantPaginationParams();
-  const { data: applicants, status } = useQuery(
+  const {
+    data: applicants,
+    status,
+    isPreviousData,
+  } = useQuery(
     ["allApplicant", { generation, order, pageIndex, searchKeyword }],
     () =>
       getApplicantByPageWithGeneration(
@@ -24,8 +29,35 @@ const ApplicantList = ({ generation }: ApplicantListProps) => {
       ),
     {
       enabled: !!generation,
+      keepPreviousData: true,
     }
   );
+  const queryClient = useQueryClient();
+  /**
+   * 2025.03.18
+   * NOTE: 해당 prefetch는 client단의 prefetch입니다.
+   * 서버사이드에서의 prefetch를 하지 않은 이유는, 검색어에 따라 상태가 자주 바뀌기 때문입니다.
+   * 검색어가 변경될 때마다 서버에서 prefetch는 낭비일 수 있습니다. 따라서 client에서 prefetch를 하는 것이 좋다고 생각합니다.
+   */
+
+  useEffect(() => {
+    if (!applicants?.maxPage) return;
+    if (!isPreviousData && applicants.maxPage > +pageIndex) {
+      queryClient.prefetchQuery({
+        queryKey: [
+          "allApplicant",
+          { generation, order, pageIndex: `${+pageIndex + 1}`, searchKeyword },
+        ],
+        queryFn: () =>
+          getApplicantByPageWithGeneration(
+            +pageIndex + 1,
+            generation,
+            order,
+            searchKeyword
+          ),
+      });
+    }
+  }, [applicants, isPreviousData, pageIndex, queryClient]);
 
   if (status === "loading") {
     return <LoadingSpinner size="s" />;
--- frontend/components/interview/InterviewerList.tsx
+++ frontend/components/interview/InterviewerList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import useInterviewerPaginationParams from "../../src/hooks/interview/useInterviewerPaginationParams";
 import { getInterviewRecordByPageWithOrder } from "../../src/apis/interview";
 import InterviewBoard from "./Board";
@@ -8,6 +8,7 @@ import InterviewPageNavbar from "./PageNavbar";
 import LoadingSpinner from "../common/LoadingSpinner";
 import { removeAll } from "../../src/functions/replacer";
 import { CHARACTERS } from "../../src/constants";
+import { useEffect } from "react";
 
 interface InterviewerListProps {
   generation: string;
@@ -16,7 +17,11 @@ interface InterviewerListProps {
 const InterviewerList = ({ generation }: InterviewerListProps) => {
   const { pageIndex, order, searchKeyword } = useInterviewerPaginationParams();
 
-  const { data: interview, status } = useQuery({
+  const {
+    data: interview,
+    status,
+    isPreviousData,
+  } = useQuery({
     queryKey: [
       "allInterviewRecord",
       { pageIndex, order, generation, searchKeyword },
@@ -29,6 +34,40 @@ const InterviewerList = ({ generation }: InterviewerListProps) => {
         searchKeyword,
       }),
   });
+
+  /**
+   * 2025.03.18
+   * NOTE: 해당 prefetch는 client단의 prefetch입니다.
+   * 서버사이드에서의 prefetch를 하지 않은 이유는, 검색어에 따라 상태가 자주 바뀌기 때문입니다.
+   * 검색어가 변경될 때마다 서버에서 prefetch는 낭비일 수 있습니다. 따라서 client에서 prefetch를 하는 것이 좋다고 생각합니다.
+   */
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (!interview?.maxPage) return;
+    if (!isPreviousData && interview.maxPage > +pageIndex) {
+      queryClient.prefetchQuery({
+        queryKey: [
+          "allInterviewRecord",
+          { pageIndex: `${+pageIndex + 1}`, order, generation, searchKeyword },
+        ],
+        queryFn: () =>
+          getInterviewRecordByPageWithOrder({
+            page: +pageIndex + 1,
+            order: order,
+            year: generation,
+            searchKeyword,
+          }),
+      });
+    }
+  }, [
+    interview?.maxPage,
+    pageIndex,
+    order,
+    generation,
+    searchKeyword,
+    queryClient,
+  ]);
 
   if (status === "loading") {
     return (
