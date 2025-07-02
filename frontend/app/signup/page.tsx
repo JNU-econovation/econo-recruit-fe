6da227c248b75b36efdd8d3b202669a0615964b0
GimGayeon <kky1373@naver.com>
2025-07-02 15:57:50 +0900

    refactor: 회원가입 요청 로직 tanstack-query 로 수정

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -2,8 +2,9 @@
 
 import SignUpForm from "@/components/user/SignUpForm.component";
 import { signUp } from "@/src/apis/user";
+import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 interface setFormAction {
   name: keyof typeof initState;
@@ -25,31 +26,46 @@ const formDataReducer = (state: typeof initState, action: setFormAction) => {
   };
 };
 
+interface SignUpData {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  username: string;
+  generation: string;
+}
+
 const SignUpPage = () => {
   const navigate = useRouter();
   const [signUpData, setForm] = useReducer(formDataReducer, initState);
-  const [error, setError] = useState("");
 
-  const onSubmit = async () => {
-    if (
-      await signUp({
-        email: signUpData.email,
-        password: signUpData.password,
-        name: signUpData.username,
-        year: +signUpData.generation,
-      })
-    ) {
-      setError("");
-      navigate.push("/signin");
+  const { mutate: submitSignUp, isLoading } = useMutation({
+    mutationFn: (data: SignUpData) =>
+      signUp({
+        email: data.email,
+        password: data.password,
+        name: data.username,
+        year: Number(data.generation),
+      }),
+
+    onSuccess: () => {
       alert("회원가입이 완료되었습니다.");
-    }
-    setError("회원가입이 실패했습니다.");
+      navigate.push("/signin");
+    },
+    onError: (error) => {
+      console.error("Sign up error:", error);
+      alert("회원가입 중 오류가 발생했습니다. 다시 시도해주세요.");
+    },
+  });
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    submitSignUp(signUpData);
   };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <SignUpForm onSubmit={onSubmit} data={signUpData} setForm={setForm} />
-      {error && <p className="text-red-500 text-sm my-2">{error}</p>}
     </div>
   );
 };
