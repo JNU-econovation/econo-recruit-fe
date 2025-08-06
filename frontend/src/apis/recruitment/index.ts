e98d5479aaff08ac780345d08ed041efbfe56a3f
조인식 <imsif42@naver.com>
2025-08-06 16:21:33 +0900

    feat: 모집 접수기간 관리 페이지 & api 통신 로직 추가

---
+++ frontend/src/apis/recruitment/index.ts
@@ -0,0 +1,53 @@
+import { https } from "../../functions/axios";
+
+export interface RecruitmentTime {
+  year: number;
+  startDate: number;
+  endDate: number;
+}
+
+export interface RecruitmentResponse {
+  endAt: string;
+  recruitmentId: number;
+  startAt: string;
+  states: "ACTIVE" | "NON_START" | "END";
+}
+
+export interface PageInfo {
+  boardLimit: number;
+  currentPage: number;
+  endPage: number;
+  listCount: number;
+  pageLimit: number;
+  startPage: number;
+}
+
+export interface RecruitmentListResponse {
+  pageInfo: PageInfo;
+  responses: RecruitmentResponse[];
+}
+
+export const postRecruitmentTime = async ({
+  year,
+  startDate,
+  endDate,
+}: RecruitmentTime) => {
+  const { data } = await https.post<string>(`/recruitment`, {
+    year,
+    startAt: startDate,
+    endAt: endDate,
+  });
+  return data;
+};
+
+export const getRecruitmentTime = async (
+  page = 1
+): Promise<RecruitmentListResponse> => {
+  const { data } = await https.get(`/page/${page}/recruitments`);
+  return data;
+};
+
+export const deleteRecruitmentTime = async (recruitmentId: string) => {
+  const { data } = await https.delete(`/recruitments/${Number(recruitmentId)}`);
+  return data;
+};
