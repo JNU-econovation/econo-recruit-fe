544ff1af5e6e73d6eab5ab1fe52e2abae167bbce
2yunseong <dbsdltjd123@gmail.com>
2025-09-03 20:30:38 +0900

    feat: 로그인 페이지 비밀번호 찾기 리다이렉트 추가
    Co-authored-by: TFOJL <dle234@users.noreply.github.com>

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -42,12 +42,19 @@ const SignInPage = () => {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
+      <img src="/images/econo-3d-logo.png" />
       <SignInForm onSubmit={onSubmit} data={signInData} setForm={setForm} />
       {error && <p className="text-red-500 text-sm my-2">{error}</p>}
-      <div className="mt-4">
-        계정이 없으신가요? &nbsp;
+      <div className="mt-4 flex gap-2">
+        <span>계정이 없으신가요?</span>
         <Link className="text-gray-400" href="/signup">
           회원가입
+        </Link>
+      </div>
+      <div className="mt-2 flex gap-2">
+        <span>비밀번호를 잊어버리셨나요?</span>
+        <Link className="text-gray-400" href="/password-reset">
+          비밀번호 찾기
         </Link>
       </div>
     </div>
